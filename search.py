b26b4bf0ff293512f1aad9b1ea643139691ed158
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-27 09:08:08 -0800

    implemented recursive linear search, need to refactor

--- search.py
+++ search.py
@@ -19,12 +19,20 @@ def linear_search_iterative(array, item):
 def linear_search_recursive(array, item, index=0):
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests below
-    if array[0] == item:
-        return index  #base case
-    elif array[index] is item:
-        return index
-    else:
+    # if array[0] == item:
+    #     return index  #base case
+    # elif array[index] is item:
+    #     return index
+    # else:
+    #     return None
+
+    try:
+        if array[index] == item:
+            return index
+    except IndexError:
         return None
+
+    return linear_search_recursive(array, item, index + 1)
 
 
 def binary_search(array, item):
